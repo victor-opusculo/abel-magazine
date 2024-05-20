e46901fe2ecf9a6f1a499fef05fd0fb9ffc13160
Victor Opusculo Oliveira Ventura de Almeida <99560606+victor-opusculo@users.noreply.github.com>
2024-05-20 14:41:39 -0300

    Update HomePage.php

--- app/HomePage.php
+++ app/HomePage.php
@@ -25,7 +25,7 @@ final class HomePage extends Component
 {
     protected function setUp()
     {
-        HeadManager::$title = "Revista de Educação Legislativa em Foco - RELF";
+        HeadManager::$title = "Revista Educação Legislativa em Foco - RELF";
         $conn = Connection::get();
         try
         {
